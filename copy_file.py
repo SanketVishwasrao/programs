1965a07ddc990bc21701b62c3319eed18473310f
sanketvishwasrao111@gmail.com <sanketvishwasrao111@gmail.com>
2024-04-01 11:20:21 +0530

    file, dict, string & list programs

--- copy_file.py
+++ copy_file.py
@@ -3,3 +3,34 @@
 from shutil import copyfile
 
 copyfile("I:/demo.txt", "I:/file.txt")
+
+# Program to Read a file line by line into a list
+
+# Solution 1: Using readlines()
+
+# f = open("file1.txt", "r")
+# lines = f.readlines()
+# print(lines)
+
+# new_lines = [x.strip() for x in lines]  # strip() method is used to remove "\n"
+# print(new_lines)
+
+# Solution 2: Using for loop and List comprehension
+
+# line = [line for line in f]
+# print(line)
+
+# new_line = [x.strip() for x in line]  # strip() method is used to remove "\n"
+# print(new_line)
+
+
+# Program to append to a file
+
+f = open("demo.txt", "a")
+f.write("\nThis is my demo file")
+
+t = "\nHope you like it"
+
+for i in range(0,5):
+    f.write(t)
+f.close()
--- string_program.py
+++ string_program.py
@@ -23,3 +23,62 @@ print("----------------------------")
 # Solution 3: Parse String Float Numeral into Integer
 
 s2 = "123.45"
+print(type(s2))
+
+str_float_int = int(float(s2))
+print(type(str_float_int))
+
+print("----------------------------")
+
+
+# Program to Convert String to Datetime
+
+# Using Datetime module
+
+from datetime import datetime
+
+date = "Oct 14 1997 7:15AM"
+print(type(date))
+
+date_time = datetime.strptime(date, "%b %d %Y %I:%M%p")
+print(date_time) 
+print(type(date_time))
+
+# Using dateutil module
+
+# from dateutil import parser
+# date_time1 = parser.parse("Oct 14 1997 7:15AM")
+# print(date_time1)
+# print(type(date_time1))
+
+
+# Program to Get a substring of a string
+
+a = "Harry Potter and the Goblet of Fire"
+print(a[:12])
+print(a[-14:-8])
+
+print("Harry Potter", end=" ")
+print("and the Goblet of Fire")
+
+
+# Program to check if a string is a number(Float)
+
+# Solution 1: Using random module
+
+# import random
+# l = [2,5,"a","y",8,"p"]
+# random_value = random.choice(l)
+# print(random_value)
+
+# Solution 2: Using secret module
+
+import secrets
+l = [2,5,"a","y",8,"p"]
+
+random_value = secrets.choice(l)
+print(random_value)
+
+
+# Program to Create a Long multiline string
+
